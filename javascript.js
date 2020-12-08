15bfde9fe25baf0784ee5b2bdc37d6bbd8392ede
Aidan <aidanbab1@gmail.com>
2020-12-08 11:43:37 -0500

    the stuff lamorie said to add

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 //this will be the function that checks if the "ball" hits the wall. If it does then we will have the ball change directions.
-function bounce(){
-  var yVel =;
-  var yVel =;
+function bounce(position){
+  var yVel = 10;//Both values wil lstart at a default speed of 10 and they will either always be positive or negitve 10
+  var yVel = 10;
+
+  if(position.x = 0)
 }
