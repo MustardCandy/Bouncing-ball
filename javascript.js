3dfb6d07bd084ee11565b5ae5585c520ae0fe0a7
Aidan <aidanbab1@gmail.com>
2020-12-08 12:04:48 -0500

    USE THIS VERSION

--- javascript.js
+++ javascript.js
@@ -1,7 +1,50 @@
-//this will be the function that checks if the "ball" hits the wall. If it does then we will have the ball change directions.
-function bounce(position){
-  var yVel = 10;//Both values wil lstart at a default speed of 10 and they will either always be positive or negitve 10
-  var yVel = 10;
+var canvas = document.getElementById("myCanvas");
+// ^^^ {id: "myCanvas", width: 400, height: 200} made from the html
+var context = canvas.getContext("2d");
 
-  if(position.x = 0)
+//var controler = setInterval(circleFunctionNoClear, 100);
+
+// function for inside the inside function
+function circleFunctionNoClear(){
+  var rad = Math.floor(Math.random() * 50);
+  var maxX = canvas.width - (rad * 2);
+  var maxY = canvas.height - (rad * 2);
+  var xVal = Math.floor(Math.random() * maxX) + rad;
+  var yVal = Math.floor(Math.random() * maxY) + rad;
+  context.beginPath();
+  context.arc(xVal, yVal, rad, 0, 2 * Math.PI);
+  context.stroke();
 }
+
+function squareFunctionNoClear(){
+  var width = Math.floor(Math.random() * 100);
+  var maxX = canvas.width - (width);
+  var maxY = canvas.height - (width);
+  var xVal = Math.floor(Math.random() * maxX);
+  var yVal = Math.floor(Math.random() * maxY);
+  context.beginPath();
+  context.rect(xVal, yVal, width, width);
+  context.stroke();
+}
+
+
+// fancy loop getaround for animating
+function circles(){
+  var interval = setInterval(step, 100);
+  var counter = 0;
+
+  function step(){ // this is within the circles namespace
+    if ((counter % 20) == 0) {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+    }
+    if ((counter % 2) == 0){
+      circleFunctionNoClear();
+    }
+    else {
+      squareFunctionNoClear();
+    }
+    counter ++;
+  }
+}
+
+circles();
